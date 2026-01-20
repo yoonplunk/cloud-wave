2d368d85bc5e7e6bd41b5e92dfff72c44e7abe12
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2026-01-20 00:30:49 +0000

    update login function

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def side_bar():
     with st.sidebar.container():
         selected_page = option_menu(
             menu_title="Main Menu", 
-            #options=["Main_Home"],
-             options=["Main_Home", "Photo_Gallery"],
-            # options=["Main_Home", "Photo_Gallery", "Travel_Attractions"],
+            # options=["Main_Home"],
+            # options=["Main_Home", "Photo_Gallery"],
+            options=["Main_Home", "Photo_Gallery", "Travel_Attractions"],
             icons=['bi bi-house', 'bi bi-images', 'bi bi-airplane-engines'],
             menu_icon="cast", 
             default_index=0,
